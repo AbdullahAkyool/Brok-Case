26ef7dfe8760a27ed7f19faf479ac5227fd0612e
Abdullah Akyol <abdullah35akyol@gmail.com>
2025-03-27 18:35:13 +0300

    Interaction System

---
+++ Assets/Scripts/Interaction System/CollectableObject.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CollectableObject : MonoBehaviour, IInteractable
+{
+    protected Transform currentHolder;
+    private Rigidbody rb;
+    private Collider clldr;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        clldr = GetComponent<Collider>();
+    }
+    public void Interact()
+    {
+        if (InteractionHandler.Instance.CurrentCollectedObject != null) return;
+
+        TakeObject(InteractionHandler.Instance.CurrentTakePoint);
+    }
+
+    public virtual void TakeObject(Transform holder)
+    {
+        if (holder == null) return;
+
+        currentHolder = holder;
+        transform.SetParent(currentHolder);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        GetRigirdBody().isKinematic = true;
+        clldr.enabled = false;
+
+        InteractionHandler.Instance.CurrentCollectedObject = this;
+    }
+
+    public virtual void DropObject()
+    {
+        transform.SetParent(null);
+        GetRigirdBody().isKinematic = false;
+        clldr.enabled = true;
+        currentHolder = null;
+    }
+
+    public Rigidbody GetRigirdBody()
+    {
+        if (rb != null)
+        {
+            return rb;
+        }
+        else
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+            return rb;
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction System/InteractionHandler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour
+{
+    public static InteractionHandler Instance { get; private set; }
+
+    [SerializeField] private KeyCode interactKey = KeyCode.E;
+    [SerializeField] private LayerMask interactableLayerMask;
+    [SerializeField] private float interactionDistance = 2f;
+
+    private Transform currentCamera;
+    private Transform currentTakePoint;
+    public Transform CurrentTakePoint => currentTakePoint;
+
+    private CollectableObject currentCollectedObject;
+    public CollectableObject CurrentCollectedObject { get => currentCollectedObject; set => currentCollectedObject = value; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetActiveCharacter(Transform cameraTransform, Transform takePoint)
+    {
+        currentCamera = cameraTransform;
+        currentTakePoint = takePoint;
+    }
+
+    private void Update()
+    {
+        if (currentCamera == null || currentTakePoint == null) return;
+
+        if (Input.GetKeyDown(interactKey))
+        {
+            Ray ray = new Ray(currentCamera.position, currentCamera.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, interactionDistance, interactableLayerMask))
+            {
+                IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+                if (interactable != null)
+                {
+                    if (currentCollectedObject != null)
+                    {
+                        currentCollectedObject.DropObject();
+                        currentCollectedObject = null;
+                    }
+
+                    interactable.Interact();
+                    return;
+                }
+            }
+
+            if (currentCollectedObject != null)
+            {
+                currentCollectedObject.DropObject();
+                currentCollectedObject = null;
+            }
+        }
+    }
+}
